30f279f9a20a2c5acd2602cafbcb117242bd9050
harumaki1220 <haruma181220@gmail.com>
2026-03-06 01:51:40 +0900

    feat: HonoからPrisma経由でDBへの書き込みに成功

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,29 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
 
-const app = new Hono()
+const app = new Hono();
+const prisma = new PrismaClient();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/users", async (c) => {
+  const users = await prisma.user.findMany();
+  return c.json(users);
+});
+
+app.get("/create-user", async (c) => {
+  const newUser = await prisma.user.create({
+    data: {
+      email: `test-${Date.now()}@example.com`,
+      name: "Test User",
+    },
+  });
+  return c.json(newUser);
+});
+
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+  port,
+});
